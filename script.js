c6c6b1066ca404d136d19bbe4980150c8341b38d
Andrew Parsons <parsonsaaa@gmail.com>
2021-03-25 16:01:06 -0700

    Added month, day, and year to header

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+var saveBtns = $('.saveBtn');
+var localStorage = $('.localStorage');
+var day = $('.currentDay');
 
 
-var dayYear = moment().format("LL");
+
+
+setInterval(function() {
+var dayYear = moment().format("MMM Do YYYY");
 $("#currentDay").text(dayYear);
+
+})
+
+saveBtns.on("click", function() {
+
+    var text = $(this).siblings('.description').val();
+    var Id = $(this).parent().attr('id');
+    localStorage.setItem(getId, text);
+});
+
+
+// function renderSchedule() {
+//     var textKey = hour10.attr("id");
+//     var value = localStorage.getItem(textKey);
+//     hour10.find(".description").val(value);
+
+// };
+// renderSchedule();
+// getting something from local storage
+// console.log key
